fdb7062adb4302559c15a8285ec28f9c7525debf
Anu Khambete <anukhambete@gmail.com>
2018-06-27 19:12:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num_1 = 5 #Kernel.rand(1..20)
+    num_1 =  Kernel.rand(1..20)
     num_2 = 5 #Kernel.rand(1..20)
     num_3 = 5 #Kernel.rand(1..20)
 
