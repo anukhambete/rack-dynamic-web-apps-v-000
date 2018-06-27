eb85ece1d8447f656b64c3d636d6541d98374cee
Anu Khambete <anukhambete@gmail.com>
2018-06-27 19:12:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,8 @@ class Application
     resp = Rack::Response.new
 
     num_1 =  Kernel.rand(1..20)
-    num_2 = 5 #Kernel.rand(1..20)
-    num_3 = 5 #Kernel.rand(1..20)
+    num_2 =  Kernel.rand(1..20)
+    num_3 =  Kernel.rand(1..20)
 
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
